e786678df5be78f4f8ba5ebc550502666ea1e94f
hjrt <kjerte@gmail.com>
2017-02-07 19:51:44 +0100

    edited avg_notes method in student_decorator.rb

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    avg = subject_item.subject_item_notes.average(:value)
+    avg.nil? ? "0.00" : '%.2f' % avg
   end
 end

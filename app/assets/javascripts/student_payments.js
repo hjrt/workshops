292d6554c110ec9c0cb262664ada171ac312eba2
hjrt <kjerte@gmail.com>
2017-02-09 18:45:31 +0100

    fixed jquery-datatable for student payments

--- app/assets/javascripts/student_payments.js
+++ app/assets/javascripts/student_payments.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$(window).on('turbolinks:load', function() {
 	$('#table-student-payments').DataTable({
 	  pagingType: 'full_numbers',
 	  columnDefs: [ {
       	targets: 4,
       	searchable: false,
       	orderable: false
-    } ]
+      } ]
 	});
 });
